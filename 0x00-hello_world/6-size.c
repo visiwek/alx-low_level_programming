5cf30df29e39b334f5eab8d354aa91eccc8d5076
visiwek <viviancsiwekwu@gmail.com>
2024-06-20 19:47:10 -0700

    bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this describes the main function of the code
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
+	printf("size of a int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
