c4c1ab9b15c6e0307191ee3f77e06a0ea30b32b4
visiwek <viviancsiwekwu@gmail.com>
2024-06-20 19:32:07 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
